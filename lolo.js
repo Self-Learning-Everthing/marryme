e7f5ce9eb867fa3779e3f84b550ea19a323e2aab
Self-Learning-Everthing <141266012+Self-Learning-Everthing@users.noreply.github.com>
2023-09-12 22:22:41 +0300

    Add files via upload

---
+++ lolo.js
@@ -0,0 +1,13 @@
+function no() {
+  alert("Wrong answer");
+}
+
+//function yes() {
+//  console.log("no");
+//}
+
+const No = document.querySelector(".no");
+//const Yess = document.querySelector(".MarryMe");
+
+No.addEventListener("click", no);
+//Yess.addEventListener("click", yes);
